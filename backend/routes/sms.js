d632441249202f10e81d932ba31a4e3e30733256
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-04-20 20:36:45 +0530

    Configuring for the customer.

--- backend/routes/sms.js
+++ backend/routes/sms.js
@@ -4,8 +4,8 @@ import twilio from "twilio";
 const router = express.Router();
 
 router.post("/send-sms", (req, res) => {
-  const accountSid = "AC615721aeff5d9003e99d20e7e96e7509";
-  const authToken = "f280a919fa06836cb6914e8e9fa37bc9";
+  const accountSid = "AC1dab5973b0ed726993e681700c60d8d9";
+  const authToken = "6fdfe93ecd7de1b41864b0b5b50f29ab";
   const client = twilio(accountSid, authToken);
 
   const { name, location, mobileNo, items, bill, date, time } = req.body;
@@ -17,7 +17,7 @@ router.post("/send-sms", (req, res) => {
       const message = await client.messages.create({
         body: `Hi ${data.name}, your order:\n ${data.items} is ready,\n Location is ${data.location}. The bill generated is ${data.bill}`,
         from: "whatsapp:+14155238886",
-        to: "whatsapp:+918433038283",
+        to: "whatsapp:+917500277575",
       });
 
       console.log(message.body);
